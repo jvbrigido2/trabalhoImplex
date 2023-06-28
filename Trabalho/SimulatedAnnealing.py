66326ae64d28bf94d0cb9d2bc539904dd3f553af
MatheusRCG <matheus.rcg.0@gmail.com>
2023-06-28 19:57:56 -0400

    SimulatedAnnealing.py nomes modificados

---
+++ Trabalho/SimulatedAnnealing.py
@@ -0,0 +1,61 @@
+import numpy
+import random
+import Utils
+
+def distanciaSolucao(grafo, solucao):
+    distancia = 0
+    for i in range(len(solucao) - 1):
+        distancia += grafo.distanciaBetweenVertices(solucao[i], solucao[i + 1])
+    return distancia
+
+
+def solucaoRandom (grafo):
+    cidades = grafo.getAllVerticesIndex().tolist()
+    solucao = []
+
+    for i in range(len(grafo.getAllVerticesIndex())):
+        cidadeRandom = cidades[random.randint(0, len(cidades) - 1)]
+        solucao.append(cidadeRandom)
+        cidades.remove(cidadeRandom)
+
+    return solucao
+
+
+def run(path, TemperaturaMAX, TemperaturaMIN, k, iteracoes):
+    grafo = Utils.creatGraph(path)
+
+    Temperatura = TemperaturaMAX
+
+    # ponto corrente de forma randomica
+    solucao = solucaoRandom(grafo)
+
+    # Simulated Annaeling
+    custo_0 = distanciaSolucao(grafo, solucao)
+
+    while Temperatura < TemperaturaMIN:
+        Temperatura = 0
+        while Temperatura != iteracoes:
+            # Troca as coordenadas e pega a solução vizinha
+            r1, r2 = numpy.random.randint(0, len(solucao), size=2)
+
+            auxiliar = solucao[r1]
+            solucao[r1] = solucao[r2]
+            solucao[r2] = auxiliar
+
+            # Pega novo custo
+            custo_1 = distanciaSolucao(grafo, solucao)
+
+            if custo_1 < custo_0:
+                custo_0 = custo_1
+            else:
+                x = numpy.random.uniform()
+                if x < numpy.exp((custo_0 - custo_1) / Temperatura):
+                    custo_0 = custo_1
+                else:
+                    auxiliar = solucao[r1]
+                    solucao[r1] = solucao[r2]
+                    solucao[r2] = auxiliar
+            Temperatura = Temperatura+1
+        Temperatura = k * Temperatura
+
+    return solucao, custo_0
